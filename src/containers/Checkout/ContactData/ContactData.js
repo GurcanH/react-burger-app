73b6ccf0856eddee951a9f7725c2a68464a36ba5
Gurcan Hamali <gurcanhamali@gmail.com>
2020-09-07 12:46:36 +1000

    280. Adjusting Checkout and Contact Data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
 import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.module.css";
 import axios from "../../../axios-orders";
@@ -93,7 +94,6 @@ class ContactData extends Component {
   };
 
   orderHandler = (event) => {
-    console.log(this.props);
     event.preventDefault();
     this.setState({ loading: true });
     const formData = {};
@@ -103,7 +103,7 @@ class ContactData extends Component {
       ].value;
     }
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData,
     };
@@ -196,4 +196,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice,
+  }
+}
+
+export default connect(mapStateToProps)(ContactData);
