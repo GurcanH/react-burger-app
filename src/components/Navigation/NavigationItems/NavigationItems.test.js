1c7a0102071aedfdaf2486c1909aa8711814242f
Gurcan Hamali <gurcanhamali@gmail.com>
2020-09-09 23:21:13 +1000

    358. Jest and Enzyme Documentations

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -20,4 +20,8 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });    
+    it('should render three <NavigationItem /> element if authemticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });      
 });
