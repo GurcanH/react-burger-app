64d79d347e80a1e594fbee7f46f5d191a6f16e5b
Gurcan Hamali <gurcanhamali@gmail.com>
2020-09-09 20:40:23 +1000

    335. Redirecting the User to the Checkout Page

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -5,6 +5,7 @@ const initialState = {
   ingredients: null,
   totalPrice: 4,
   error: false,
+  building: false
 };
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -24,6 +25,7 @@ const addIngredient = (state, action) => {
       const updatedState = {
         ingredients: updatedIngredients,
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        building: true
       };
       return updateObject(state, updatedState);
 }
@@ -36,6 +38,7 @@ const removeIngredient = (state, action) => {
       const updatedSt = {
         ingredients: updatedIngs,
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        building: true
       };
       return updateObject(state, updatedSt);
 }
@@ -49,6 +52,7 @@ const setIngredient = (state, action) => {
         },
         totalPrice: 4,
         error: false,
+        building: false
       });
 }
 
