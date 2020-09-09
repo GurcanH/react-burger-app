01d84a70251454bc937a30c8582c084e4fb33f8c
Gurcan Hamali <gurcanhamali@gmail.com>
2020-09-09 23:08:04 +1000

    356. Writing our First Test

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,19 +1,20 @@
-import React from "react";
-import classes from "./Toolbar.module.css";
-import Logo from "../../Logo/Logo";
-import NavigationItems from "../NavigationItems/NavigationItems";
-import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+import React from 'react';
 
-const toolbar = (props) => (
-  <header className={classes.Toolbar}>
-    <DrawerToggle clicked={props.drawerToggleClicked} />
-    <div className={classes.Logo}>
-      <Logo />
-    </div>
-    <nav className={classes.DesktopOnly}>
-      <NavigationItems isAuthenticated={props.isAuth} />
-    </nav>
-  </header>
+import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
+const toolbar = ( props ) => (
+    <header className={classes.Toolbar}>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems isAuthenticated={props.isAuth} />
+        </nav>
+    </header>
 );
 
 export default toolbar;
