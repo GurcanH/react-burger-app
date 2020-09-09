992741d445240c96c8f3c8e1e954786e1497420d
Gurcan Hamali <gurcanhamali@gmail.com>
2020-09-09 22:36:04 +1000

    350. Wrap Up

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,29 +1,29 @@
-import React from "react";
-import Logo from "../../Logo/Logo";
-import NavigationItems from "../NavigationItems/NavigationItems";
-import classes from "./SideDrawer.module.css";
-import Backdrop from "../../UI/Backdrop/Backdrop";
-import Auxi from "../../../hoc/Auxi/Auxi";
+import React from 'react';
 
-const sideDrawer = (props) => {
-  let attachedClasses = [classes.SideDrawer, classes.Close];
-  if (props.open){
-    attachedClasses = [classes.SideDrawer, classes.Open];
-  }
-  return (
-    <Auxi>
-      <Backdrop show = {props.open} clicked = {props.closed}/>
-      <div className={attachedClasses.join(" ")} onClick={props.closed}>
-        <div className={classes.Logo}>
-          <Logo />
-        </div>
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxi/Auxi';
 
-        <nav>
-          <NavigationItems isAuthenticated={props.isAuth}/>
-        </nav>
-      </div>
-    </Auxi>
-  );
+const sideDrawer = ( props ) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')} onClick={props.closed}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems isAuthenticated={props.isAuth} />
+                </nav>
+            </div>
+        </Aux>
+    );
 };
 
 export default sideDrawer;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -11,7 +11,7 @@ class Modal extends Component {
   }
 
   componentWillUpdate () {
-    console.log('[Modal] componentWillUpdate');
+    // console.log('[Modal] componentWillUpdate');
   }
 
   render() {
